748f6e03daebbb7890636124a69c2ef8d597612d
MariahS <smith.mariah@live.com>
2019-05-11 11:04:45 -0400

    Setting up controllers, domain, and services

---
+++ src/main/java/domain/Subreddit.java
@@ -0,0 +1,41 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Subreddit {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="Id")
+    private Integer id;
+
+    @Column(name="Name")
+    private String name;
+
+    @Column(name="Url")
+    private String url;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/services/RedditServiceImpl.java
@@ -0,0 +1,4 @@
+package services;
+
+public class RedditServiceImpl implements RedditService {
+}
